2e8e2e846ab95c5b04b3022c952a663127dec48f
Ben D'Agnillo <bdagnillo@gmail.com>
2026-04-20 16:07:41 -0400

    Update test_vesc.py

--- altairfc/tests/test_vesc.py
+++ altairfc/tests/test_vesc.py
@@ -3,9 +3,10 @@ from pyvesc import VESC
 
 with VESC(serial_port='/dev/ttyACM0', baudrate=500000, start_heartbeat=False, timeout=0.5) as motor:
     time.sleep(0.5)
+    motor.set_duty_cycle(0.1)
     for _ in range(10):
-        motor.set_duty_cycle(0.1)
         time.sleep(1)
+        motor.set_duty_cycle(0.1)
         meas = motor.get_measurements()
         print(f"RPM: {meas.rpm}, Voltage: {meas.v_in}V, Current: {meas.avg_motor_current}A")
     motor.set_current(0)
