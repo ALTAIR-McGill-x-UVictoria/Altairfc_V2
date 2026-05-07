acb14763e8f871b7528c0ab16b0a0227335ae4cd
Ben D'Agnillo <bdagnillo@gmail.com>
2026-05-07 19:03:09 -0400

    Update heartbeat.py

--- altairfc/telemetry/packets/heartbeat.py
+++ altairfc/telemetry/packets/heartbeat.py
@@ -45,7 +45,7 @@ class HeartbeatPacket:
         "system.pps_rms_us"
     """
 
-    TX_RATE_HZ: ClassVar[float] = 2.0
+    TX_RATE_HZ: ClassVar[float] = 4.0
 
     DATASTORE_KEYS: ClassVar[dict[str, str]] = {
         "time_unix":            "system.time_unix",
